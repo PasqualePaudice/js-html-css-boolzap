2fba3c60a7707a4f45ed7f98a89ea8235ff2b311
Pasquale Paudice <pasquale.paudice10@gmail.com>
2019-12-05 23:02:27 +0100

    fix

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+
+    var messaggio1 = $('.messaggio.template').clone();
+
+    messaggio1.removeClass('template').addClass('ricevuto');
+
+    messaggio1.children('.testo-messaggio').text('ciao,come va?');
+
+    messaggio1.find('.delete').css('backgroundColor','white');
+    messaggio1.find('.delete').css('box-shadow','none');
+
+    $('.schermata-messaggi').append(messaggio1);
+
     var messaggio = $('.messaggio.template').clone();
 
     messaggio.removeClass('template').addClass('mandato');
@@ -7,6 +19,11 @@ $(document).ready(function(){
     messaggio.children('.testo-messaggio').text('ciao');
 
     $('.schermata-messaggi').append(messaggio);
+
+
+
+
+
 
 
 
@@ -41,42 +58,15 @@ $(document).ready(function(){
     });
 
 // se clicchi sul bottone invia, invia il messaggio e rimuove le classi precedentemente aggiunte al bottone invia e al microfono, facendo ricomparire quest ultimo.
+    $(document).on('click', '.button', function(){
+        scrivi_messaggio();
+    })
 
-    $('.button').click(function(){
+    $(".ricerca input").keyup(function(){
 
+        ricerca();
 
-        var messaggio_inviato = $('.main.visible .messaggio-da-inviare').val();
-
-        var messaggio = $('.messaggio.template').clone();
-
-        messaggio.removeClass('template').addClass('mandato');
-
-        messaggio.children('.testo-messaggio').text(messaggio_inviato);
-
-        $('.main.visible .schermata-messaggi').append(messaggio);
-
-
-        $('.messaggio-da-inviare').val('');
-
-        $('.mic').removeClass('invisible');
-        $('.button').removeClass('visible');
-
-
-        //funzione che permette di ricevere il messaggio "ok" dopo il nostro invio
-        setTimeout(function(){
-
-        var messaggioRicevuto = $('.messaggio.ricevuto:first-child').clone();
-
-        messaggioRicevuto.appendTo('.main.visible .schermata-messaggi').html('ok');
-
-
-        },1000);
-
-
-
-
-    }
-    );
+  });
 
     $(".scrivi-messaggio").keypress(function(e) {
 
@@ -100,7 +90,16 @@ $(document).ready(function(){
     });
 
 
-    $('.anteprima-chat').click(function(){
+
+
+
+
+
+    $('.tutte_chat .anteprima-chat').click(function(){
+
+
+        $('.anteprima-chat').removeClass('active');
+        $(this).addClass('active');
 
         $('.main').removeClass('visible');
         $('.main').addClass('invisible');
@@ -125,39 +124,61 @@ $(document).ready(function(){
 
 
 
-    function scrivi_messaggio(){
-
-        var messaggio_inviato = $('.main.visible .messaggio-da-inviare').val();
-
-        var messaggio = $('.messaggio.template').clone();
-
-        messaggio.removeClass('template').addClass('mandato');
-
-        messaggio.children('.testo-messaggio').text(messaggio_inviato);
-
-        $('.main.visible .schermata-messaggi').append(messaggio);
-
-
-        $('.messaggio-da-inviare').val('');
-
-        $('.mic').removeClass('invisible');
-        $('.button').removeClass('visible');
-
-
-        //funzione che permette di ricevere il messaggio "ok" dopo il nostro invio
-        setTimeout(function(){
-
-        var messaggioRicevuto = $('.messaggio.ricevuto:first-child').clone();
-
-        messaggioRicevuto.appendTo('.main.visible .schermata-messaggi').html('ok');
-
-
-        },1000);
 
 
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+function scrivi_messaggio(){
+
+    var messaggio_inviato = $('.main.visible .messaggio-da-inviare').val();
+
+    var messaggio = $('.messaggio.template').clone();
+
+    messaggio.removeClass('template').addClass('mandato');
+
+    messaggio.children('.testo-messaggio').text(messaggio_inviato);
+
+    $('.main.visible .schermata-messaggi').append(messaggio);
+
+
+    $('.messaggio-da-inviare').val('');
+
+    $('.mic').removeClass('invisible');
+    $('.button').removeClass('visible');
+
+
+    //funzione che permette di ricevere il messaggio "ok" dopo il nostro invio
+    setTimeout(function(){
+
+    var messaggioRicevuto = $('.messaggio.template').clone();
+
+    messaggioRicevuto.removeClass('template').addClass('ricevuto')
+    messaggioRicevuto.appendTo('.main.visible .schermata-messaggi').html('ok');
+
+
+    },1000);
+
+    $('.tutte_chat .anteprima-chat.active').prependTo('.tutte_chat');
+
+
+
+
+}
 
 
 
@@ -169,64 +190,47 @@ $(document).ready(function(){
 
 
 // Funzione di ricerca dei contatti
-    function ricerca(){
-
-
-
-        var nome_cercato = $('.casella-ricerca input').val();
-
-
-        var nome_da_cercare = $(' .tutte_chat .nome strong');
-
-        nome_da_cercare.each(function(){
-
-            nome_singolo = $(this).text();
-
-
-            if(nome_cercato.toLowerCase() != 0){
-
-            if (nome_cercato.toLowerCase() == nome_singolo.toLowerCase() || nome_singolo.toLowerCase().includes(nome_cercato.toLowerCase())){
-
-                $(this).closest('.anteprima-chat').addClass('visible');
-                $(this).closest('.anteprima-chat').removeClass('invisible');
-
-                console.log('si');
-
-
-            }else{
-                $(this).closest('.anteprima-chat').addClass('invisible');
-                $(this).closest('.anteprima-chat').removeClass('visible');
-
-                console.log('no');
-
-            }}else{
-                $(this).closest('.anteprima-chat').addClass('visible');
-                $(this).closest('.anteprima-chat').removeClass('invisible');
-            }
-
-        })
+function ricerca(){
 
 
 
 
 
-    }
+    var nome_cercato = $('.casella-ricerca input').val();
+
+
+    var nome_da_cercare = $(' .tutte_chat .nome strong');
+
+    nome_da_cercare.each(function(){
+
+        nome_singolo = $(this).text();
+
+
+        if(nome_cercato.toLowerCase() != 0){
+
+        if (nome_cercato.toLowerCase() == nome_singolo.toLowerCase() || nome_singolo.toLowerCase().includes(nome_cercato.toLowerCase())){
+
+            $(this).closest('.anteprima-chat').addClass('visible');
+            $(this).closest('.anteprima-chat').removeClass('invisible');
+
+            console.log('si');
+
+
+        }else{
+            $(this).closest('.anteprima-chat').addClass('invisible');
+            $(this).closest('.anteprima-chat').removeClass('visible');
+
+            console.log('no');
+
+        }}else{
+            $(this).closest('.anteprima-chat').addClass('visible');
+            $(this).closest('.anteprima-chat').removeClass('invisible');
+        }
+
+    })
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+}
